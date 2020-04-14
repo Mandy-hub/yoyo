2b8bfd35e90c98b8dcfc3579e79b62607412b3dd
Mandy-hub <507336088@qq.com>
2020-04-14 22:38:43 +0800

    ee

--- yoyo_jiekou/config/readConfig.py
+++ yoyo_jiekou/config/readConfig.py
@@ -7,6 +7,7 @@ import ConfigParser
 
 # 当前文件夹的路径
 cur_path = os.path.dirname(os.path.realpath(__file__))
+#获得cfg.ini的绝对路径
 configPath = os.path.join(cur_path, "cfg.ini")
 
 conf = ConfigParser.ConfigParser()
